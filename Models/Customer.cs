c76e612a1c02e7b4e76ea4f10a41f99290d23d3d
stephon nelson <stephon.nelson81@gmail.com>
2021-02-19 12:53:02 -0600

    identiy review scaffolding setting up role manger class done

---
+++ Models/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("Member Id")]
+        public string Name { get; set; }
+        [DisplayName("Name")]
+        public string SuperHeroAlterEgo { get; set; }
+        [DisplayName("Primary Abilities")]
+        public string Primarysuperheroability { get; set; }
+        [DisplayName("Secondary Abilities")]
+        public string Secondarysuperheroability { get; set; }
+        [DisplayName("Catch Phrase")]
+        public string CatchPhrase { get; set; }
+    }
+}
---
+++ Models/Pickups.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector.Models
+{
+    public class Pickups
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("")]
+        public string SuperHeroName { get; set; }
+        [DisplayName("Super Hero Alter Ego ")]
+        public string SuperHeroAlterEgo { get; set; }
+        [DisplayName("Primary Abilities")]
+        public string Primarysuperheroability { get; set; }
+        [DisplayName("Secondary Abilities")]
+        public string Secondarysuperheroability { get; set; }
+        [DisplayName("Catch Phrase")]
+        public string CatchPhrase { get; set; }
+    }
+}
